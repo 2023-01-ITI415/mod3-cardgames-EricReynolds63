36b677ef55722db83cdabac4c034d332723f68bc
EricReynolds63 <err63@scarletmail.rutgers.edu>
2023-03-10 21:09:14 -0500

    Adding Prospector P1
    AKA the rest of the owl
    Started adding prospector elements with Prospector and CardProspector

--- Assets/Prospector/__Scripts/Deck.cs
+++ Assets/Prospector/__Scripts/Deck.cs
@@ -18,10 +18,14 @@ public class Deck : MonoBehaviour {
  
     static public GameObject SPRITE_PREFAB { get; private set; }
  
+	/*
+
     void Start() {
         InitDeck();
         Shuffle(ref cards);
     }
+
+	*/
  
     /// <summary>
     /// The Prospector class will call InitDeck to set up the deck and build
