7e103072e49a6d00fa06abdd6b6e6eff003c32eb
EricReynolds63 <err63@scarletmail.rutgers.edu>
2023-04-06 19:10:39 -0400

    Combining Part 2 & 3 Step 1
    Copied Part 2 assets folder off Github (as ZIP), added to project, hooked up main scene buttons

---
+++ Assets/Loader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("__Prospector_Scene_0");
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("__Pyramid_Scene_0");
+    }
+
+}
